a9bd4a0d48352065071e8a13d9388fc12e65c7e3
sang5c <skah321@gmail.com>
2021-08-02 21:29:58 +0900

    ch01: 상속을 이용한 관심사 분리

---
+++ src/main/java/com/example/toby/ch01/dao/NUserDao.java
@@ -0,0 +1,14 @@
+package com.example.toby.ch01.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class NUserDao extends UserDao {
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "sa", "");
+    }
+
+}
---
+++ src/main/java/com/example/toby/ch01/dao/UserDao.java
@@ -0,0 +1,47 @@
+package com.example.toby.ch01.dao;
+
+import com.example.toby.ch01.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public abstract class UserDao {
+    public abstract Connection getConnection() throws SQLException;
+
+    public void add(User user) throws SQLException {
+        Connection c = getConnection();
+
+        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws SQLException {
+        Connection c = getConnection();
+
+        PreparedStatement ps = c.prepareStatement("select * from users where id=?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+
+}
